6099727c18789dc651b541b0aa08ef8284fd6f7a
Jorin Vermeulen <jorin.vermeulen@gmail.com>
2015-12-17 19:58:35 +0100

    updated scripts

--- xorinzor/shoutzor/assets/liquidsoap/vlc.sh
+++ xorinzor/shoutzor/assets/liquidsoap/vlc.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-file="big_buck_bunny_1080p_h264.mov"
-#file="Traced.mp3"
-#fallbackvideo="placeholder.ogg"
-logo="shoutzor-christmas-logo-small.png"
+file="../images/placeholder.ogg"
+logo="../images/shoutzor-christmas-logo-small.png"
 transparency=255 #0 = fully transparent, 255 = fully opaque
-threads=3
+threads=4
 vcodec=theo
 acodec=vorb
+videoquality=10 #0 lowest, 10 highest
+audioquality=10 #0 lowest, 10 highest
 width=1920
 height=1080
+destination="localhost"
+port=8000
+password="hackme"
 
 vlc "$file" \
 --loop \
 --quiet \
 --width 1920 --height 1080 \
---sout "#transcode{sfilter=logo{file='$logo',x=5,y=5,transparency=$transparency},deinterlace,hq,threads=$threads,vcodec=$vcodec,acodec=$acodec,ab=192,channels=2,width=$width,height=$height}:std{access=shout,mux=ogg,dst=source:vledder@localhost:8000/vlc.ogg}" --sout-keep
+--sout-theora-quality=$videoquality \
+--sout-vorbis-quality=$audioquality \
+--sout "#transcode{sfilter=logo{file='$logo',x=5,y=5,transparency=$transparency},deinterlace,hq,threads=$threads,vcodec=$vcodec,acodec=$acodec,ab=192,channels=2,width=$width,height=$height}:std{access=shout,mux=ogg,dst=source:$password@$destination:$port/vlc.ogg}" --sout-keep
