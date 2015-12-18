85f9897dddbb4702793695ddd937b86fa0954018
Jorin Vermeulen <jorin.vermeulen@gmail.com>
2015-12-18 21:40:31 +0100

    vlc script fix

--- xorinzor/shoutzor/assets/scripts/vlc.sh
+++ xorinzor/shoutzor/assets/scripts/vlc.sh
@@ -2,11 +2,11 @@
 
 placeholder="../images/placeholder.ogg"
 logo="../images/shoutzor-christmas-logo-small.png"
-logo_x_pos = 5
-logo_y_pos = 5
-logo_transparency = 255
-telnetport = 4212
-telnetpassword = "hackme"
+logo_x_pos=5
+logo_y_pos=5
+logo_transparency=255
+telnetport=4212
+telnetpassword="hackme"
 threads=4
 vcodec=theo
 acodec=vorb
@@ -14,9 +14,10 @@ videoquality=10 #0 lowest, 10 highest
 audioquality=10 #0 lowest, 10 highest
 width=1920
 height=1080
-destination="localhost"
-port=8000
-password="hackme"
+output_destination="localhost"
+output_port=8000
+output_password="hackme"
+output_mount="shoutzor.ogg"
 
 vlc "$placeholder" \
 --ttl 12 \
