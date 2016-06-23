eebab8dc3bc3c3866fc1c3f73a4c32c86a415176
Xorinzor <jorin.vermeulen@gmail.com>
2016-06-23 19:22:30 +0200

    Using proper references in FormGenerator now

--- xorinzor/shoutzor/src/App/FormBuilder/FormGenerator.php
+++ xorinzor/shoutzor/src/App/FormBuilder/FormGenerator.php
@@ -10,15 +10,11 @@ class FormGenerator {
     private $fields = array();
     private $fieldPointer = null;
 
-    public function __construct()
-    {
-    }
-
     /**
      * Sets the pointer to the desired field
      */
     public function selectField($id) {
-        $this->fieldPointer = $id;
+        $this->fieldPointer = $this->fields[$id];
     }
 
     /**
@@ -26,42 +22,42 @@ class FormGenerator {
      */
     public function addField(FormField $field) {
         $this->fields[$field->getId()] = $field;
-        $this->fieldPointer = $field->getId();
+        $this->fieldPointer = $field;
     }
 
     /**
      * Sets the type of the currently selected field
      */
     public function setFieldName($name) {
-        $this->fields[$this->fieldPointer]->setName($name);
+        $this->fieldPointer->setName($name);
     }
 
     /**
      * Sets the type of the currently selected field
      */
     public function setFieldType($type) {
-        $this->fields[$this->fieldPointer]->setType($type);
+        $this->fieldPointer->setType($type);
     }
 
     /**
      * Sets the title of the currently selected field
      */
     public function setFieldTitle($title) {
-        $this->fields[$this->fieldPointer]->setTitle($title);
+        $this->fieldPointer->setTitle($title);
     }
 
     /**
      * Sets the value of the currently selected field
      */
     public function setFieldValue($value) {
-        $this->fields[$this->fieldPointer]->setValue($value);
+        $this->fieldPointer->setValue($value);
     }
 
     /**
      * Sets the description of the currently selected field
      */
     public function setFieldDescription($description) {
-        $this->fields[$this->fieldPointer]->setDescription($description);
+        $this->fieldPointer->setDescription($description);
     }
 
     private function buildField(FormField $field) {
